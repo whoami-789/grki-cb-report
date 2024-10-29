f7b2a928432faaeb281595edad32b21a2b4a3e95
whoami-789 <tudya1238@gmail.com>
2024-10-29 15:12:32 +0500

    data change

--- src/main/java/com/grkicbreport/dto/saveAgreement/saveAgreementDTO.java
+++ src/main/java/com/grkicbreport/dto/saveAgreement/saveAgreementDTO.java
@@ -1,13 +1,73 @@
 package com.grkicbreport.dto.saveAgreement;
 
 import com.grkicbreport.dto.CreditorDTO;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
 public class saveAgreementDTO {
     private String save_mode;
     private CreditorDTO creditor;
     private AgreementDTO agreement;
+
+    public saveAgreementDTO() {
+    }
+
+    public String getSave_mode() {
+        return this.save_mode;
+    }
+
+    public CreditorDTO getCreditor() {
+        return this.creditor;
+    }
+
+    public AgreementDTO getAgreement() {
+        return this.agreement;
+    }
+
+    public void setSave_mode(String save_mode) {
+        this.save_mode = save_mode;
+    }
+
+    public void setCreditor(CreditorDTO creditor) {
+        this.creditor = creditor;
+    }
+
+    public void setAgreement(AgreementDTO agreement) {
+        this.agreement = agreement;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof saveAgreementDTO)) return false;
+        final saveAgreementDTO other = (saveAgreementDTO) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$save_mode = this.getSave_mode();
+        final Object other$save_mode = other.getSave_mode();
+        if (this$save_mode == null ? other$save_mode != null : !this$save_mode.equals(other$save_mode)) return false;
+        final Object this$creditor = this.getCreditor();
+        final Object other$creditor = other.getCreditor();
+        if (this$creditor == null ? other$creditor != null : !this$creditor.equals(other$creditor)) return false;
+        final Object this$agreement = this.getAgreement();
+        final Object other$agreement = other.getAgreement();
+        if (this$agreement == null ? other$agreement != null : !this$agreement.equals(other$agreement)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof saveAgreementDTO;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $save_mode = this.getSave_mode();
+        result = result * PRIME + ($save_mode == null ? 43 : $save_mode.hashCode());
+        final Object $creditor = this.getCreditor();
+        result = result * PRIME + ($creditor == null ? 43 : $creditor.hashCode());
+        final Object $agreement = this.getAgreement();
+        result = result * PRIME + ($agreement == null ? 43 : $agreement.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "saveAgreementDTO(save_mode=" + this.getSave_mode() + ", creditor=" + this.getCreditor() + ", agreement=" + this.getAgreement() + ")";
+    }
 }
--- src/main/java/com/grkicbreport/dto/saveContract/ClaimDTO.java
+++ src/main/java/com/grkicbreport/dto/saveContract/ClaimDTO.java
@@ -1,14 +1,88 @@
 package com.grkicbreport.dto.saveContract;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class ClaimDTO {
     private String claim_guid;
     private String claim_id;
     private String contract_guid;
     private String contract_id;
+
+    public ClaimDTO() {
+    }
+
+    public String getClaim_guid() {
+        return this.claim_guid;
+    }
+
+    public String getClaim_id() {
+        return this.claim_id;
+    }
+
+    public String getContract_guid() {
+        return this.contract_guid;
+    }
+
+    public String getContract_id() {
+        return this.contract_id;
+    }
+
+    public void setClaim_guid(String claim_guid) {
+        this.claim_guid = claim_guid;
+    }
+
+    public void setClaim_id(String claim_id) {
+        this.claim_id = claim_id;
+    }
+
+    public void setContract_guid(String contract_guid) {
+        this.contract_guid = contract_guid;
+    }
+
+    public void setContract_id(String contract_id) {
+        this.contract_id = contract_id;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof ClaimDTO)) return false;
+        final ClaimDTO other = (ClaimDTO) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$claim_guid = this.getClaim_guid();
+        final Object other$claim_guid = other.getClaim_guid();
+        if (this$claim_guid == null ? other$claim_guid != null : !this$claim_guid.equals(other$claim_guid))
+            return false;
+        final Object this$claim_id = this.getClaim_id();
+        final Object other$claim_id = other.getClaim_id();
+        if (this$claim_id == null ? other$claim_id != null : !this$claim_id.equals(other$claim_id)) return false;
+        final Object this$contract_guid = this.getContract_guid();
+        final Object other$contract_guid = other.getContract_guid();
+        if (this$contract_guid == null ? other$contract_guid != null : !this$contract_guid.equals(other$contract_guid))
+            return false;
+        final Object this$contract_id = this.getContract_id();
+        final Object other$contract_id = other.getContract_id();
+        if (this$contract_id == null ? other$contract_id != null : !this$contract_id.equals(other$contract_id))
+            return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof ClaimDTO;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $claim_guid = this.getClaim_guid();
+        result = result * PRIME + ($claim_guid == null ? 43 : $claim_guid.hashCode());
+        final Object $claim_id = this.getClaim_id();
+        result = result * PRIME + ($claim_id == null ? 43 : $claim_id.hashCode());
+        final Object $contract_guid = this.getContract_guid();
+        result = result * PRIME + ($contract_guid == null ? 43 : $contract_guid.hashCode());
+        final Object $contract_id = this.getContract_id();
+        result = result * PRIME + ($contract_id == null ? 43 : $contract_id.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "ClaimDTO(claim_guid=" + this.getClaim_guid() + ", claim_id=" + this.getClaim_id() + ", contract_guid=" + this.getContract_guid() + ", contract_id=" + this.getContract_id() + ")";
+    }
 }
