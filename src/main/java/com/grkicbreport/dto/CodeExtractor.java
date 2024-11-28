cf0f12928019c1042ffcf209fde6fab95c13b836
whoami-789 <tudya1238@gmail.com>
2024-11-28 15:34:57 +0500

    data change

--- src/main/java/com/grkicbreport/dto/CodeExtractor.java
+++ src/main/java/com/grkicbreport/dto/CodeExtractor.java
@@ -7,7 +7,7 @@ public class CodeExtractor {
 
     public static String extractCode(String input) {
         // Регулярное выражение для поиска кода в строке
-        String regex = "\\d{6,}\\w*[-/]?\\d{0,2}";
+        String regex = "\\d+[A-Za-zА-Яа-я]+/\\d{4}";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
