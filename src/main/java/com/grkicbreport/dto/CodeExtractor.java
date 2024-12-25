5c4917494199678f50a037880c9d5b4cf75a4696
whoami-789 <tudya1238@gmail.com>
2024-12-25 11:53:58 +0500

    14999

--- src/main/java/com/grkicbreport/dto/CodeExtractor.java
+++ src/main/java/com/grkicbreport/dto/CodeExtractor.java
@@ -7,7 +7,7 @@ public class CodeExtractor {
 
     public static String extractCode(String input) {
         // Регулярное выражение для поиска кода в строке
-        String regex = "\\b\\d+\\b"; // Находит последовательности цифр, окруженные границами слова
+        String regex = "\\b\\d+(?:/\\d+)?\\b"; // Находит числа, включая числа с косой чертой
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
